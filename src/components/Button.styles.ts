277f936e91115cb357e61230918c624851d4dcde
Hyago Mesquita <bhyagomesquita@gmail.com>
2024-10-24 23:55:04 -0300

    feat: adicionar tipagem de temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
    border: 0;
    margin: 8px;
 
-   background-color: ${props => props.theme.asdfgsfgh};
+   background-color: ${props => props.theme.primary};
    color: ${props => props.theme.white}
 
    /* ${props => {
